1457f37c8e47831d2fad66b812c2c755ea11b866
Alexandru Lupasco <alexandru5345@gmail.com>
2022-02-03 17:30:25 +0200

    update lesson

--- src/Lesson4homework/MinutesConverter.java
+++ src/Lesson4homework/MinutesConverter.java
@@ -3,8 +3,24 @@ package Lesson4homework;
 import java.util.Scanner;
 
 public class MinutesConverter {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter minutes: ");
+        long min = input.nextLong();
+
+        //long min = 5464648;
+        double days ;
+        double years ;
+        days = min / 1440f;
+        years = min / 525600f;
+        input.close();
+
+        String days1 = String.format("Days = " + "%.2f", days);
+        String years1 = String.format("Years = "+ "%.2f", years);
+        System.out.println(days1);
+        System.out.println(years1);
+
     }
 
 }
